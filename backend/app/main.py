cf109a8c68a364691bda088f77b7410eb6d360b4
Jazp7 <aristideszelayax@gmail.com>
2026-03-12 23:21:00 -0600

    less origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -29,10 +29,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://stocklink-nine.vercel.app",
-        "http://stocklink-nine.vercel.app"
+        "https://stocklink-nine.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
